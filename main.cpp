b7959e55b2fa29bc5d7390b6bf986648b9b95a89
Nany <125378399+Nany033@users.noreply.github.com>
2023-09-06 12:09:35 -0600

    ya está javi

--- main.cpp
+++ main.cpp
@@ -62,6 +62,60 @@ int findString(string text, string find){
     return -1;
 
 }
+
+string transString(const string& text1){
+
+    string result = "#";
+    
+    //transform the string for it to include # at every position including beggining and end
+    for (size_t i = 0; i < text1.size()+1; ++i) {
+        result += text1[i]; 
+        
+        if (i < text1.size() - 1) {
+            result += "#";
+        }
+    }
+    
+    return result;
+}
+
+string Manacher(const string& text1){
+    
+    string transformed= transString(text1);
+    int n = text1.size();
+    vector<int> pal(n, 0);
+    int center=0, right=0;
+
+    for (int i = 1; i < n - 1; i++) {
+        int mirror = 2 * center - i;// calculate the mirror of i with center
+
+        if (i < right) {
+            pal[i] = min(right - i, pal[mirror]);
+        }
+
+        while (transformed[i + pal[i] + 1] == transformed[i - pal[i] - 1]) {
+            pal[i]++;
+        }
+
+        if (i + pal[i] > right) {
+            center = i;
+            right = i + pal[i];
+        }
+    }
+    int maxLen = 0;
+    int centerIndex = 0;
+
+    for (int i = 1; i < n - 1; i++) {
+        if (pal[i] > maxLen) {
+            maxLen = pal[i];
+            centerIndex = i;
+        }
+    }
+
+    int start = (centerIndex - maxLen) / 2;
+    return text1.substr(start, maxLen);
+}
+
 pair<int, int> findLongestCommonSubstring(const string& text1, const string& text2) {
     int m = text1.size();//length of text1
     int n = text2.size();//length of text2
@@ -96,6 +150,7 @@ int main(){
     string m2code=readFile("mcode2.txt");
     string m3code=readFile("mcode3.txt");
     int m;
+    string pal;
     pair<int, int> c;
     // transmission1.txt
     m=findString(transmission1,m1code);
@@ -112,6 +167,15 @@ int main(){
     cout<<((m>=0) ? "True "+to_string(m+1): "False ")<<endl;
     m=findString(transmission2,m3code);
     cout<<((m>=0) ? "True "+to_string(m+1): "False ")<<endl;
+
+    //transmission1.txt
+    pal= Manacher(transmission1);
+    cout << pal << " transmission1"  << endl;
+
+    //transmission2.txt
+    pal= Manacher(transmission2);
+    cout << pal << " transmission2"  << endl;
+
     
     c = findLongestCommonSubstring(transmission1, transmission2);
     cout<<c.first<<" "<<c.second<<endl;
